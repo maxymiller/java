a90578229f577ddb4fa43991e2cd692b700a6e89
maxymiller <152033482+maxymiller@users.noreply.github.com>
2026-03-26 00:37:33 -0300

    Add files via upload

---
+++ ListMatriz.java
@@ -0,0 +1,23 @@
+public class ListMatriz {
+    int x;
+    int y;
+    int matriz;
+
+    ListMatriz(int x, int y, int matriz) {
+        this.x = x;
+        this.y = y;
+        this.matriz = matriz;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getMatriz() {
+        return matriz;
+    }
+}
