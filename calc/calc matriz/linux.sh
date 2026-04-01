76411a418cb139350515cd106498ca3eba6f1679
maxymiller <152033482+maxymiller@users.noreply.github.com>
2026-04-01 12:31:14 -0300

    .

---
+++ calc/calc matriz/linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+homepwd="$PWD"
+username="$USER"
+nul="$homepwd/tmp/nul"
+echo "bem vindo user $username code java maxymiller e java"
+cd maxymiller-windows-all
+if javac --version; then
+javac Main.java
+else
+echo "[error]: javac"
+fi
+if java --version; then
+java Main
+else
+echo "[error]: java"
+echo "[error]: download jdk java"
+read
+fi
